b5d929c6a5cc872cf6750570cd0fc4eacd813b07
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-12 21:57:57 -0800

    completed question 5

--- app.js
+++ app.js
@@ -77,3 +77,22 @@ function sumArray(testArray){ //eslint-disable-line
 testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+
+/////////////////////////////////////
+/* Problem 5
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"The numbers 2,3,4 have a product of 24."
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+
+// Write your code here
+function multiplyArray(testArray) { //eslint-disable-line
+  var multi = multiply(testArray [0], testArray[1]) [0];
+  var multiplication = multiply (multi, testArray [2]) [0];
+  var array = [multiplication, testArray [0] + ',' + testArray [1] + ',' + testArray [2] + 'was passed in as an array of numbers, and ' + multiplication + 'is their sum.'];
+  return array;
+}
+
+// Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(2,3,4);
